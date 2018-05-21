a1bda9e740c939790f3bbfb98765a9a5f8e00ca5
A-Marzouk <perfect_ahmed@yahoo.com>
2018-05-21 10:10:46 +0300

    design of workforce homepage

---
+++ includes/navbar.php
@@ -0,0 +1,20 @@
+<div id="navBar">
+    <nav class="navbar navbar-expand-lg customNav">
+        <a class="navbar-brand" href="/">
+            <img src="images/new_theme/123wf_logo.png" alt="logo" width="78%">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link customNavLink active" href="#">Homepage</a>
+                <a class="nav-item nav-link customNavLink" href="#">Become a client</a>
+                <a class="nav-item nav-link customNavLink" href="#">Talk to sales</a>
+            </div>
+        </div>
+        <ul class="pull-right nav navbar-nav">
+            <li><a href="#" class="loginBtn">Login</a></li>
+        </ul>
+    </nav>
+</div>
